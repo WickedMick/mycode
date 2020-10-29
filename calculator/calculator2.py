58f039b596a8e86935ffc4dc101ae5e59f90231f
Mick F <r.f.farrell13@gmail.com>
2020-10-29 20:41:17 +0000

    Network Interface

---
+++ calculator/calculator2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+#def add(x, y):
+    return x + y
+
+#def subtract(x, y):
+#    return x - y
+
+#def multiply(x, y):
+#   return x * y
+
+#def divide(x, y):
+#    return x / y
+
+#selection of operation
+
+#print("Select operation")
+#print("1.Add")
+#print("2.Subtract")
+
+x= float(input("First number?"))
+y= float(input("Second Number?"))
+
+sum = float(x) + float(y)
+print (sum)
+   
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
---
+++ netfacd/interface_reader.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import netifaces
+
+for i in netifaces.interfaces():    
+    print('\n**************Details of Interface - ' + i + ' *********************')
+    try:
+        print(netifaces.ifaddresses(i)[netifaces.AF_LINK][0]['addr'])
+        print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+    except:
+        print("Could not collect adapter information")
